5b420e6142d118e72ed6541a9a21615f1cdd01bb
Dmitry <upssq@mail.ru>
2021-06-06 18:32:16 +0300

    Добавил папку main.js

---
+++ source-1/main.js
@@ -0,0 +1,108 @@
+const isPositiveNumber = (value) => typeof value === 'number' && value >= 0;
+const getRandomFloat = (...args) => {
+  const [min, max, dec] = args;
+  const errorIndex = args.findIndex((value) => !isPositiveNumber(value));
+  if (errorIndex >= 0) {
+    throw new Error(`Неверный тип по индексу ${errorIndex}.`);
+  }
+  const pow = Math.pow(10, dec);
+  return Math.round((Math.random() * (max - min) + min) * pow) / pow;
+};
+
+getRandomFloat(2, 5, 2);
+
+const getRandomNumber = (min, max) => getRandomFloat(min, max, 0);
+
+getRandomNumber(2, 5);
+
+const LINK_MIN = 1;
+const LINK_MAX = 8;
+const createAuthor = () => ({
+  avatar: `img/avatars/user0${getRandomNumber(LINK_MIN, LINK_MAX)}.png`,
+});
+
+const NUMBER_MIN = 1;
+const NUMBER_MAX = 100;
+
+const createNumber = () => getRandomNumber(NUMBER_MIN, NUMBER_MAX);
+
+const TYPE = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+  'hotel',
+];
+
+const CHECKIN = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+
+const CHECKOUT = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+
+const creatFields = (element) => element[getRandomNumber(NUMBER_MIN, element.length)];
+
+const FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner',
+];
+
+const PHOTOS = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+];
+
+const createArrayRandom = (element) => {
+  const random = () => Math.random() < 0.5;
+  const array = element.filter(random);
+  if (array < 1) {
+    Math.random() * (element.length);
+  }
+  return array;
+};
+
+const createOffer = () => ({
+  title: 'Уютная квартира',
+  address: 'location.x, location.y',
+  price: createNumber(),
+  type: creatFields(TYPE),
+  rooms: createNumber(),
+  guests: createNumber(),
+  checkin: creatFields(CHECKIN),
+  checkout: creatFields(CHECKOUT),
+  features: createArrayRandom(FEATURES),
+  description: 'Офигительное жильё',
+  photos: createArrayRandom(PHOTOS),
+});
+
+const NUMBER_FILDS = 12;
+
+const offer = new Array(NUMBER_FILDS).fill(null).map(() => createOffer());
+
+const LOCATION_LAT_MIN = 35.65000;
+const LOCATION_LAT_MAX = 35.70000;
+const LOCATION_LNG_MIN = 139.70000;
+const LOCATION_LNG_MAX = 139.80000;
+const LIMIT_SINGS = 5;
+const createLocation = () => ({
+  lat: getRandomFloat(LOCATION_LAT_MIN, LOCATION_LAT_MAX, LIMIT_SINGS),
+  lng: getRandomFloat(LOCATION_LNG_MIN, LOCATION_LNG_MAX, LIMIT_SINGS),
+});
+
+const createAdv = () => ({
+  author: createAuthor(),
+  offer: createOffer(),
+  location: createLocation(),
+});
+createAdv();
