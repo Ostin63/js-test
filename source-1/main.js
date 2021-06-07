287822410f3979285b48d53920079d1d11498603
Dmitry <upssq@mail.ru>
2021-06-07 14:53:12 +0300

    Добавил функции сборки данных

--- source-1/main.js
+++ source-1/main.js
@@ -1,3 +1,59 @@
+const MIN_INDEX = 0;
+const NUMBER_MIN = 1;
+const ROOM_MAX = 3;
+const GUESTS_MAX = 3;
+const AVATAR_LENGTH = 8;
+const PRICE_MAX = 1000000;
+const LOCATION_LAT_MIN = 35.65000;
+const LOCATION_LAT_MAX = 35.70000;
+const LOCATION_LNG_MIN = 139.70000;
+const LOCATION_LNG_MAX = 139.80000;
+const LIMIT_SINGS = 5;
+const TITLES = [
+  'Уютная квартира',
+  'Квартира в ценре города',
+  'Квартира в тихом районе',
+  'Квартира не далеко от метро',
+  'Квартира рядом школа и детский сад',
+];
+const TYPES = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+  'hotel',
+]
+const CHECKINS = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+const CHECKOUTS = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+const FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner',
+];
+const DESCRIPTIONS = [
+  'Офигительное жильё',
+  'Лучше небывает',
+  'Сделан ремотн',
+  'Заменена сантехника',
+  'Все соседи умерли',
+];
+const PHOTOS = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+];
+
 const isPositiveNumber = (value) => typeof value === 'number' && value >= 0;
 const getRandomFloat = (...args) => {
   const [min, max, dec] = args;
@@ -8,56 +64,12 @@ const getRandomFloat = (...args) => {
   const pow = Math.pow(10, dec);
   return Math.round((Math.random() * (max - min) + min) * pow) / pow;
 };
-getRandomFloat(2, 5, 2);
-
 const getRandomNumber = (min, max) => getRandomFloat(min, max, 0);
-getRandomNumber(2, 5);
 
-const NUMBER_MIN = 1;
-const LINK_MAX = 8;
-const createLinkPhotoAuthor = () => ({
-  avatar: `img/avatars/user0${getRandomNumber(NUMBER_MIN, LINK_MAX)}.png`,
-});
+const padLeft = (index) => String(index).padStart(2, '0');
+const createAuthorUrl = (index) => `img/avatars/user${padLeft(index)}.png`;
 
-const NUMBER_MAX = 100;
-const createNumber = () => getRandomNumber(NUMBER_MIN, NUMBER_MAX);
-
-const TYPE = [
-  'palace',
-  'flat',
-  'house',
-  'bungalow',
-  'hotel',
-];
-
-const CHECKIN = [
-  '12:00',
-  '13:00',
-  '14:00',
-];
-
-const CHECKOUT = [
-  '12:00',
-  '13:00',
-  '14:00',
-];
-
-const creatFieldsRandom = (element) => element[getRandomNumber(NUMBER_MIN, element.length)];
-
-const FEATURES = [
-  'wifi',
-  'dishwasher',
-  'parking',
-  'washer',
-  'elevator',
-  'conditioner',
-];
-
-const PHOTOS = [
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
-];
+const getRandomItem = (element) => element[Math.floor(Math.random() * element.length)];
 
 const createArrayRandom = (element) => {
   const random = () => Math.random() < 0.5;
@@ -68,33 +80,30 @@ const createArrayRandom = (element) => {
   return array;
 };
 
-const createListOffer = () => ({
-  title: 'Уютная квартира',
-  address: 'location.x, location.y',
-  price: createNumber(),
-  type: creatFieldsRandom(TYPE),
-  rooms: createNumber(),
-  guests: createNumber(),
-  checkin: creatFieldsRandom(CHECKIN),
-  checkout: creatFieldsRandom(CHECKOUT),
-  features: createArrayRandom(FEATURES),
-  description: 'Офигительное жильё',
-  photos: createArrayRandom(PHOTOS),
-});
-
-const LOCATION_LAT_MIN = 35.65000;
-const LOCATION_LAT_MAX = 35.70000;
-const LOCATION_LNG_MIN = 139.70000;
-const LOCATION_LNG_MAX = 139.80000;
-const LIMIT_SINGS = 5;
 const createLocationСoordinates = () => ({
   lat: getRandomFloat(LOCATION_LAT_MIN, LOCATION_LAT_MAX, LIMIT_SINGS),
   lng: getRandomFloat(LOCATION_LNG_MIN, LOCATION_LNG_MAX, LIMIT_SINGS),
 });
 
-const createAdv = () => ({
-  author: createLinkPhotoAuthor(),
+const getAddress = () => `${createLocationСoordinates().lat}.` + 'x' + `,${createLocationСoordinates().lng}.` + 'y';
+
+const createListOffer = () => ({
+  title: getRandomItem(TITLES),
+  address: getAddress(),
+  price: getRandomNumber(MIN_INDEX, PRICE_MAX),
+  type: getRandomItem(TYPES),
+  rooms: getRandomNumber(NUMBER_MIN, ROOM_MAX),
+  guests: getRandomNumber(NUMBER_MIN, GUESTS_MAX),
+  checkin: getRandomItem(CHECKINS),
+  checkout: getRandomItem(CHECKOUTS),
+  features: createArrayRandom(FEATURES),
+  description: getRandomItem(DESCRIPTIONS),
+  photos: createArrayRandom(PHOTOS),
+});
+
+const getData = () => ({
+  avtor: createAuthorUrl(AVATAR_LENGTH),
   offer: createListOffer(),
   location: createLocationСoordinates(),
 });
-createAdv();
+getData();
